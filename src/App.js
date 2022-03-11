bc2532681d5562fc4b320bf9a3d448cbced18b1a
Joao Celso <joaocelso01@gmail.com>
2022-03-11 11:22:41 -0300

    useReducer + useContext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
-import { useReducer } from 'react';
+import P from 'prop-types';
+import { createContext, useContext, useReducer, useRef } from 'react';
 import './App.css';
 
-const globalState = {
-  title: 'O titulo do contexto',
+// actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+};
+
+// data.js
+export const globalState = {
+  title: 'O título que contexto',
   body: 'O body do contexto',
   counter: 0,
 };
 
-const reducer = (state, action) => {
+// reducer.js
+export const reducer = (state, action) => {
   switch (action.type) {
-    case 'muda': {
-      console.log('chamou muda');
-      return { ...state, title: 'mudou' };
-    }
-    case 'invert': {
-      console.log('chamou invert');
-      return { ...state, title: title.split('').reverse().join('') };
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar título');
+      return { ...state, title: action.payload };
     }
   }
+
+  return { ...state };
 };
 
-function App() {
+// AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globalState);
-  const { counter, title, body } = state;
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
+  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <button onCLick={() => dispatch({ type: 'muda' })}>Click</button>
-      <button onCLick={() => dispatch({ type: 'invert' })}>Click</button>
-    </div>
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{context.state.title}</h1>
+      <input type="text" ref={inputRef} />
+    </>
+  );
+};
+
+// App.jsx
+function App() {
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
