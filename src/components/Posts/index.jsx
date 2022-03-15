30e741d85f98146fa2d1d14a140a6949cb4b553b
Joao Celso <joaocelso01@gmail.com>
2022-03-15 11:54:05 -0300

    Organizaçao com useEffect, useReducer, useContext e useRef

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useRef } from 'react';
+import { PostsContext } from '../../contexts/PostsProvider/context';
+import { loadPosts } from '../../contexts/PostsProvider/actions';
+import { CounterContext } from '../../contexts/CounterProvider/context';
+import { decrementCounter, incrementCounter } from '../../contexts/CounterProvider/action';
+
+export const Posts = () => {
+  const isMounted = useRef(true);
+  const postsContext = useContext(PostsContext);
+  const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
+
+  useEffect(() => {
+    loadPosts(postsDispatch).then((dispatch) => {
+      if (isMounted.current) {
+        dispatch();
+      }
+    });
+    return () => {
+      isMounted.current = false;
+    };
+  }, [postsDispatch]);
+
+  return (
+    <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>Counter+ {counterState.counter}</button>
+      <button onClick={() => decrementCounter(counterDispatch)}>Counter- {counterState.counter}</button>
+      <h1>POSTS</h1>
+      {postsState.loading && (
+        <p>
+          <strong>Carregando posts...</strong>
+        </p>
+      )}
+
+      {postsState.posts.map((p) => (
+        <p key={p.id}>{p.title}</p>
+      ))}
+    </div>
+  );
+};
---
+++ src/contexts/CounterProvider/action.js
@@ -0,0 +1,8 @@
+import * as types from './types';
+
+export const incrementCounter = (dispatch) => {
+  dispatch({ type: types.INCREMENT_COUNTER });
+};
+export const decrementCounter = (dispatch) => {
+  dispatch({ type: types.DECREMENT_COUNTER });
+};
